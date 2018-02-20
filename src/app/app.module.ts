882a769d6a8508cc3228e09a52b7f0170aec9d7f
Kai Klostermann <kai_klostermann@elitemail.org>
2018-02-20 20:54:02 +0100

    all the features are present now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatToolbarModule, MatCardModule, MatProgressSpinnerModule, MatIconModule} from '@angular/material';
+import {MatButtonModule, MatToolbarModule, MatCardModule, MatProgressSpinnerModule, MatIconModule, MatTooltipModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { EpisodeGeneratorComponent } from './episode-generator/episode-generator.component';
 import { EpisodeService } from './episode.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EpisodeGeneratorComponent
+    EpisodeGeneratorComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +21,9 @@ import { EpisodeService } from './episode.service';
     MatToolbarModule,
     MatCardModule,
     MatProgressSpinnerModule,
-    MatIconModule
+    MatIconModule,
+    AppRoutingModule,
+    MatTooltipModule
   ],
   providers: [EpisodeService],
   bootstrap: [AppComponent]
